073c116135e3e539d1c5b6dd3765cbe92a305e07
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-09-04 21:49:04 +0300

    add arrays of different variables

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ var algorithm = function (){
   century = document.getElementById("enterMonth").value;
   dayOfWeek = (((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+ day-2)%7;
   return(Math.floor(dayOfWeek));
+  
 }
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
